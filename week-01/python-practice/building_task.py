cc449aa5e5c80bbafcf067724708a0212d9c7185
Sergei Radkevich <87006877+Radbo@users.noreply.github.com>
2026-04-12 14:21:33 +0800

    improved building_task: added input validation with if/else

--- week-01/python-practice/building_task.py
+++ week-01/python-practice/building_task.py
@@ -1,7 +1,10 @@
-user_flat = int(input("Please, enter flat number "))
-user_entrance = (((user_flat - 1) // 20) + 1)
-user_flat_number_in_entrance = (((user_flat - 1) % 20) + 1)
-user_floor = (((user_flat_number_in_entrance - 1) // 4) + 1)
+user_flat = int(input("Please, enter flat number: "))
 
-print("Entrance number",user_entrance)
-print("Floor number", user_floor)
+if user_flat <= 0 or user_flat > 100:
+    print("Please, enter flat number from 1 to 100")
+else:
+    user_entrance = (((user_flat - 1) // 20) + 1)
+    user_flat_number_in_entrance = (((user_flat - 1) % 20) + 1)
+    user_floor = (((user_flat_number_in_entrance - 1) // 4) + 1)
+    print("Entrance number", user_entrance)
+    print("Floor number", user_floor)
