3247c4e0cfbf3e9b86c44bf060f91663b965ed62
Sergei Radkevich <87006877+Radbo@users.noreply.github.com>
2026-04-12 18:13:52 +0800

    improve flat number input validation

--- week-01/python-practice/building_task.py
+++ week-01/python-practice/building_task.py
@@ -1,10 +1,14 @@
-user_flat = int(input("Please, enter flat number: "))
+user_flat = (input("Please, enter flat number: ").strip())
 
-if user_flat <= 0 or user_flat > 100:
-    print("Please, enter flat number from 1 to 100")
+if user_flat.isdigit():
+    user_flat = int(user_flat)
+    if user_flat > 0 and user_flat <= 100:
+        user_entrance = (((user_flat - 1) // 20) + 1)
+        user_flat_number_in_entrance = (((user_flat - 1) % 20) + 1)
+        user_floor = (((user_flat_number_in_entrance - 1) // 4) + 1)
+        print("Entrance number", user_entrance)
+        print("Floor number", user_floor)
+    else:
+        print("Please, enter flat number from 1 to 100")
 else:
-    user_entrance = (((user_flat - 1) // 20) + 1)
-    user_flat_number_in_entrance = (((user_flat - 1) % 20) + 1)
-    user_floor = (((user_flat_number_in_entrance - 1) // 4) + 1)
-    print("Entrance number", user_entrance)
-    print("Floor number", user_floor)
+    print("Please, enter flat number")
