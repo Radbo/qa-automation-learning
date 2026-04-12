f3103ab21a56d6293b948039d188079d5cf56924
Sergei Radkevich <87006877+Radbo@users.noreply.github.com>
2026-04-12 14:44:38 +0800

    add leap year exercise

---
+++ week-01/python-practice/leap_year_task.py
@@ -0,0 +1,8 @@
+user_year = int(input("Please, enter the year: "))
+
+if user_year % 4 != 0:
+    print("This is not a leap year")
+elif user_year % 100 == 0 and user_year % 400 != 0:
+    print("This is not a leap year")
+else:
+    print("This is a leap year")ad
