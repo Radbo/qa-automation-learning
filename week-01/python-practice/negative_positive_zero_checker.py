269c5883f70e3fe9007affd0e0b49d9c09f1dc11
Sergei Radkevich <87006877+Radbo@users.noreply.github.com>
2026-04-14 10:59:29 +0800

    add day 05 python consolidation practice

---
+++ week-01/python-practice/negative_positive_zero_checker.py
@@ -0,0 +1,8 @@
+user_number = int(input("Enter number:"))
+
+if user_number < 0:
+    print("Your number is negative")
+elif user_number > 0:
+    print("Your number is positive")
+else:
+    print("Your number is zero")
