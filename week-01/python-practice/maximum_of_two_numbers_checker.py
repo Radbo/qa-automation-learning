506064fc810d4cfdc7a2021ae4618d395a2fd51b
Sergei Radkevich <87006877+Radbo@users.noreply.github.com>
2026-04-14 13:52:27 +0800

    add maximum of two numbers checker

---
+++ week-01/python-practice/maximum_of_two_numbers_checker.py
@@ -0,0 +1,9 @@
+first_number = int(input("Enter first number: "))
+second_number = int(input("Enter second number: "))
+
+if first_number == second_number:
+    print("The numbers are equal")
+elif first_number > second_number:
+    print("The first number is greater than the second")
+else:
+    print("The second number is greater than the first")
